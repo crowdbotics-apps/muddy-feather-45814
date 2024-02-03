70b2e7d9a5a5f4b8cf2fcddf34df7a10ce51d351
Crowdbotics <team@crowdbotics.com>
2024-02-03 05:46:14 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "muddy-feather-45814",
+  url: "https://muddy-feather-45814.botics.co",
+  api: "https://muddy-feather-45814.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
